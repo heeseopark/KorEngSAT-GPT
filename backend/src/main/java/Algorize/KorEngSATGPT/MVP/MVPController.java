d9a6408af948914a0b59b459afc76ec428fb0003
heeseopark <heeseopark@korea.ac.kr>
2023-09-29 23:43:31 +0900

    Feat: coding mvp controller

--- backend/src/main/java/Algorize/KorEngSATGPT/MVP/MVPController.java
+++ backend/src/main/java/Algorize/KorEngSATGPT/MVP/MVPController.java
@@ -1,11 +1,22 @@
 package Algorize.KorEngSATGPT.MVP;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
+@RestController
+@RequestMapping("/api")
 public class MVPController {
-    @GetMapping
 
+    @PostMapping("/uploadImage")
+    public ResponseEntity<String> uploadImage(@RequestParam("image") MultipartFile file) {
+        // Handle the uploaded image file
+        // You can call the service layer here to process the uploaded file
+
+        // Return a response
+        return ResponseEntity.ok("Image uploaded successfully!"); // You can modify the response as needed
+    }
 }
